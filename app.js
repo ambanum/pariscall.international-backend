b342327ab64e0b8cb8e7f80c22d24636788729b6
Nicolas Dupont <npg.dupont@gmail.com>
2019-10-21 17:14:15 +0200

    Add views and support for pug template engine

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
